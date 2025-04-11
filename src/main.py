43889a60578e1135289ffb70cf6f18c29534e7c9
AbisaitReynaga <abisait.49@gmail.com>
2025-04-10 19:37:26 -0600

    Added UI screenshots

--- src/main.py
+++ src/main.py
@@ -2,8 +2,7 @@ import flet as ft
 from templates.MainLayout import MainLayout
 
 def main(page: ft.Page):
-    page.window_width = 900
-    page.window_height = 600
+    page.title="UI structure"
     page.add(MainLayout())
 
 ft.app(target=main)
